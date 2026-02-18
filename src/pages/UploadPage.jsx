c02a4b79df8d90e0c2b9d33cc3cca08384dc5ba5
Z3NA <zanaboss@live.dk>
2026-02-18 16:01:34 +0100

    wavebackground

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import styles from '../components/UploadPage.module.css';
-import WaveBackground from './WaveBackground';
 
 export default function UploadPage({ token }) {
   const [file, setFile] = useState(null);
